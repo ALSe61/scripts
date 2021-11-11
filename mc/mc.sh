efcd189189f1915490179e7e7d70f4ff761fae91
alse61 <alse61@gmail.com>
2021-11-11 22:44:51 +0500

    mc install script add

--- mc/mc.sh
+++ mc/mc.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 P="mc"
-
-
-if [ "$(dpkg-query -W -f="${Status}" "$P" 2>/dev/null | grep -c "ok installed")" -eq 0 ];
-then
-  apt-get install nano;
+if [ "$(dpkg-query -W -f="${Status}" "$P" 2>/dev/null | grep -c "ok installed")" -eq 0 ]; then
+  sudo apt-get install -y "$P"
 fi
-git clone 
+git clone https://github.com/ALSe61/scripts.git /tmp/scripts
+cp -f /tmp/scripts/mc/ini ~/.config/mc/ini
+if [ ! -e ~/.local/share/mc/skins ]; then
+    mkdir ~/.local/share/mc/skins
+fi
+cp -f /tmp/scripts/mc/skins/* ~/.local/share/mc/skins/
+rm -fr /tmp/scripts
+exit 0
