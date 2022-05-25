ee9e0b98566aaa6156dccc476c8525e39c01846a
ALSe61 <alse61@gmail.com>
2022-05-25 15:49:31 +0500

    update zsh.sh

--- oh-my-zsh/zsh.sh
+++ oh-my-zsh/zsh.sh
@@ -1,30 +1,26 @@
-#!/bin/sh
-
-ZSH=~/.oh-my-zsh
-# sh -c "$(wget https://raw.githubusercontent.com/ALSe61/scripts/master/oh-my-zsh/zsh.sh -O -)"
-[ -x "$(command -v apt)" ] && PM="apt"
-[ -x "$(command -v opkg)" ] && PM="opkg"
-[ -x "$(command -v pkg)" ] && PM="pkg"
+#!/bin/bash
 echo "OhMyZSH install..."
-sleep 2
-[ ! -x "$(command -v zsh)" ] && $PM update && $PM install zsh -y
-[ ! -x "$(command -v git)" ] && $PM update && $PM install git -y
-[ ! -x "$(command -v wget)" ] && $PM update && $PM install wget -y
+ZSH="$HOME/.oh-my-zsh"
+ZSH_CUSTOM="$ZSH/custom"
+# bash -c "$(wget https://raw.githubusercontent.com/ALSe61/scripts/master/oh-my-zsh/zsh.sh -O -)"
+[ -x "$(command -v apt)" ] && apt -qy install zsh 2>&1
+[ -x "$(command -v opkg)" ] && opkg -qy install zsh 2>&1
+[ -x "$(command -v pkg)" ] && pkg -qy install zsh 2>&1
 echo "Copy plugins & settings..."
-sleep 2
 # Oh-my-zsh
-[ ! -d ~/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-wget https://raw.githubusercontent.com/ALSe61/scripts/master/oh-my-zsh/.zshrc -O ~/.zshrc
-[ "$PM" = "pkg" ] && sed -i 's/export ZSH=.*/export ZSH="\/data\/data\/com.termux\/files\/home\/.oh-my-zsh"/' ~/.zshrc
+[ ! -d "$ZSH" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+wget https://raw.githubusercontent.com/ALSe61/scripts/master/oh-my-zsh/.zshrc -O ~/.zshrc 2>&1
+# sed -i 's/export ZSH=.*/export ZSH="\/data\/data\/com.termux\/files\/home\/.oh-my-zsh"/' ~/.zshrc
 # powerlevel10k
-[ ! -d ~/.oh-my-zsh/custom/themes/powerlevel10k ] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k 
-wget https://raw.githubusercontent.com/ALSe61/scripts/master/oh-my-zsh/.p10k.zsh -O ~/.p10k.zsh
+[ ! -d $ZSH_CUSTUM/themes/powerlevel10k ] && \
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k 2>&1
+wget https://raw.githubusercontent.com/ALSe61/scripts/master/oh-my-zsh/.p10k.zsh -O ~/.p10k.zsh 2>&1
 # zsh-syntax-highlighting
-[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+[ ! -d ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting ] && \
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" 2>&1
 # zsh-autosuggestions
-[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
+[ ! -d "${ZSH_CUSTOM}/plugins/zsh-autosuggestions" ] && \
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM}/plugins/zsh-autosuggestions" 2>&1
 #chsh -s zsh
 echo "Complete!"
 exit 0
